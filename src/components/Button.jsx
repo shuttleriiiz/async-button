839aedc44d9c5c8779dff019e7230810666fe384
Erlend Powell <erlendp@datacom.co.nz>
2021-04-20 11:38:41 +1200

    Added solution using useRef to handle mount state.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,40 @@
+import {useState, useEffect, useRef} from 'react'
 import React from 'react'
 import cn from 'classnames'
 
 export const Button = (props) => {
+  const [isLoading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const isMountedRef = useRef(null);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    return () => {
+      isMountedRef.current = false;
+    }
+  }, []);
+
+  const handleClick = async () => {
+    setLoading(true);
+    setHasError(false);
+
+    try {
+      await props.onClick();
+    }
+    catch (err) {
+      if (isMountedRef.current) {
+        setHasError(true);
+      }
+    }
+
+    if (isMountedRef.current) {
+      setLoading(false);
+    }
+  };
+
   return (
-    <button className={cn('button', {'is-loading': false, 'is-danger': false })} {...props} />
+    <button onClick={handleClick} className={cn('button', {'is-loading': isLoading, 'is-danger': hasError })} children={props.children} />
   )
 }
